3a73a974c27f92166aaa143e772f9bda310fe3ea
fishedafisherman <fishedafisherman@gmail.com>
2024-10-02 21:39:08 -0700

    lets commit this then add the img's that this totorial uses

--- slidepuzzle.js
+++ slidepuzzle.js
@@ -30,3 +30,54 @@ window.onload = function() {
         }
     }
 }
+
+function dragStart() {
+    currTime = this; //This refers to the img tile being dragged
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragENter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTime = this; //this refers to the img tile being dopped on
+}
+
+function dragEnd() {
+
+    if(!otherTile.src.includes("3.jpg")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = r == r2 && c2 == c-1;
+    let moveRight = r == r2 && c2 == c+1;
+
+    let moveUp = c == c2 && r2 == r-1;
+    let moveDown = c == c2 && r2 == r+1;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+    
+    if (isAdjacent) {
+        let currImg = currTime.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+    }
+}
